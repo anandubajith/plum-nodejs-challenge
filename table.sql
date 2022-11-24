fced57c16638b422ed007558e834a4bf0c3c3719
Anandu B Ajith <reach2anandu@gmail.com>
2022-11-24 16:24:17 +0530

    stricter table schema

--- table.sql
+++ table.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS organizations (
     id serial primary key,
-    name varchar(255) not null,
+    name varchar(255) not null
 );
+
+CREATE TYPE gender_enum AS ENUM ('male', 'female', 'other');
 
 CREATE TABLE IF NOT EXISTS members (
     id serial primary key,
@@ -12,16 +14,15 @@ CREATE TABLE IF NOT EXISTS members (
     middle_name varchar(255),
     last_name varchar(255) not null,
     date_of_birth varchar(255) not null,
-    gender varchar(10) not null,
-    CONSTRAINT fk_member FOREIGN KEY(organization_id) REFERENCES organizations(id)
+    gender gender_enum not null,
+    CONSTRAINT fk_member FOREIGN KEY(organization_id) REFERENCES organizations(id),
     UNIQUE (organization_id , employee_id)
 )
 
 
 INSERT INTO organizations(name) VALUES ('Test');
-INSERT INTO organizations(name) VALUES ('Test2');
-INSERT INTO organizations(name) VALUES ('Test3');
-INSERT INTO  organizations(name) VALUES ('Testr5');
 
 INSERT INTO members(organization_id, employee_id, email, first_name, middle_name, last_name, date_of_birth, gender)
-    VALUES(1, 123, 'test@gmail.com', 'test', null, 'test', '12/12/1999' ,'Male')
+    VALUES(1, 123, 'test@gmail.com', 'test', null, 'test', '12/12/1999' ,'male')
+INSERT INTO members(organization_id, employee_id, email, first_name, middle_name, last_name, date_of_birth, gender)
+    VALUES(1, 124, 'test2@gmail.com', 'test2', null, 'test2', '12/12/1999' ,'female')
